c0db75627e6dc2b19ee7493365e2aa9f6e8c6c78
mrtkrkrt <muratkrkrt0711@gmail.com>
2021-10-26 15:12:07 +0300

    setting up html partials

--- app.js
+++ app.js
@@ -6,10 +6,18 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/add', (req, res) => {
+    res.render('add');
 });
 
 app.listen(port, () => {});
