2e10f82684734a2d33e55d85a7711b59ead3b515
mrtkrkrt <muratkrkrt0711@gmail.com>
2021-10-30 16:49:35 +0300

    database

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/PCAT', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
 const port = 3000;
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -18,6 +28,11 @@ app.get('/about', (req, res) => {
 
 app.get('/add', (req, res) => {
     res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+    await Photo.create(req.body);
+    res.redirect('/');
 });
 
 app.listen(port, () => {});
---
+++ models/Photo.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const GameSchema = new Schema({
+    name: String,
+    player_num: String,
+    Description: String,
+    image: String,
+    dateCreated: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Photo = mongoose.model('Game', GameSchema);
+module.exports = Photo;
